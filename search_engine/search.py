b88ce7d92bd6222353b2516045ced259ffa48978
Cameron Ryan <cjryanwashere@gmail.com>
2024-06-18 10:50:39 -0400

    starting to work on gRPC search app

--- search_engine/search.py
+++ search_engine/search.py
@@ -21,15 +21,23 @@ class SearchEngineClient:
         # load the vector index clients
         self.open_clip_image_client = vector_index_client.VectorIndexClient("open_clip_image", hostname="localhost")
 
+        self.snowflake_arctic_s_client = vector_index_client.VectorIndexClient("snowflake_arctic_s", hostname="localhost")
+
         # load the embedding providers
-        self.open_clip_embedding_provider = embedding_provider.EmbeddingProvider("open_clip")
+        #self.open_clip_embedding_provider = embedding_provider.EmbeddingProvider("open_clip")
+
+        self.snowflake_arctic_s_embedding_provider = embedding_provider.EmbeddingProvider("snowflake_arctic_s")
     
     def search_image(self, image: Image) -> List[vector_index.SearchResult]:
         image_embedding = self.open_clip_embedding_provider.open_clip_embed_image(image)
         return self.open_clip_image_client.search(image_embedding)
 
     def search_text(self, text: str) -> List[vector_index.SearchResult]:
-        pass
+
+        text_embedding = self.snowflake_arctic_s_embedding_provider.embed_text(text)
+
+        return self.snowflake_arctic_s_client.search(text_embedding)
+        
 
 if __name__ == "__main__":
     pass
