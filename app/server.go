87f55592ab1e8ae20fceed18bc5b78ef37a8b8b6
Cameron Ryan <cjryanwashere@gmail.com>
2024-06-19 20:02:13 -0400

    working with go server

--- app/server.go
+++ app/server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"os"
+	"fmt"
 
 	"net/http"
 	"github.com/labstack/echo/v4"
@@ -11,11 +12,13 @@ import (
 func main () {
 	e := echo.New()
 	e.GET("/", func (c echo.Context) error {
+		fmt.Printf("recieved search request")
 		content, err := os.ReadFile("search_page.html")
 		if err != nil {
 			log.Fatal(err)
 		}
 		return c.HTML(http.StatusOK, string(content))
+
 	})
 
 	e.Logger.Fatal(e.Start(":1323"))
